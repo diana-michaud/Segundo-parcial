8a5892e7c4b53f63c72099a4cc664a53c3630e8c
Diana Michaud <michaud.diana.hm@gmail.com>
2026-09-22 12:21:18 -0600

    Trabjo en clase terminado: Mena y Diana

--- Trabajo en pareja en clase/Program.cs
+++ Trabajo en pareja en clase/Program.cs
@@ -27,52 +27,159 @@
 
 
 //Mostrar la vida de las jugadoras (no puede nunca ser mayor a 67 o menor a 0) (Jugadora 1 y 2: ataque aleatorio)
+Console.WriteLine("\n");
 Console.WriteLine("Hola jugadoras, hoy combatiran a muerte, ambas epezarán con 67 porciento de vida, decidiran, en cada turno, si quieren atacar o curarse, quien logrte reducir la vida de la otra a 0 es la ganadora");
 Console.WriteLine ("\n");
 Console.WriteLine ("Ingresa el nombre de la jugadora 1:");
-string jugadora1 = Console.ReadLine();
-int vidajugadora1 = 67;
-Random v = new Random();
-var y = v.Next(10, 21);
-int ataque1 = v.Next(10, 21);
+string jugadora1      = Console.ReadLine();
+int    vidajugadora1  = 67;
+Random v              = new Random();
+var    y              = v.Next(10, 21);
+int    ataque1        = v.Next(10, 21);
 Console.WriteLine("El ataque de " + jugadora1 + " es: " + ataque1);
 int curacion1 = v.Next(10, 21);
 Console.WriteLine("La curacion de " + jugadora1 + " es: " + curacion1);
+Console.WriteLine("\n");
 Console.WriteLine ("Ingresa el nombre de la jugadora 2:");
-string jugadora2 = Console.ReadLine();
-int vidajugadora2 = 67;
-int ataque2 = v.Next(10, 21);
+string jugadora2     = Console.ReadLine();
+int    vidajugadora2 = 67;
+int    vidajugadora22;
+int    ataque2 = v.Next(10, 21);
 Console.WriteLine("El ataque de " + jugadora2 + " es: " + ataque2);
 int curacion2 = v.Next(10, 21);
 Console.WriteLine("La curacion de " + jugadora2 + " es: " + curacion2);
 Console.WriteLine ("\n");
 Console.WriteLine ("¡Empezamos!, da enter para continuar");
+Console.WriteLine("\n\n");
 Console.ReadLine();
 var r = new Random();
 var x = r.Next(1, 3);
 
-if (x == 1)
-{
-    Console.WriteLine( jugadora1 + " Comienza");
-}
-else
-{
-    Console.WriteLine( jugadora2 + " Comienza");
-    Console.WriteLine(jugadora2 + "Deseas atacar o curarte");
-    string aoc =Console.ReadLine();
-
-    if (aoc == "atacar")
-    {
-        vidajugadora1 = vidajugadora1 - ataque2;
-    }
-    else if(aoc == "curar")
-
-    {
-        vidajugadora2 = vidajugadora2 +
-    }
-
-}
-
 //Elegir quien inicia (aleatorio)
+while (vidajugadora1 > 0 && vidajugadora2 > 0)
+{
 
+    if (x == 1)
+    {       Console.WriteLine("\n");
+        Console.WriteLine(jugadora1 + " Comienza");
+        Console.WriteLine("\n");
+        Console.WriteLine(jugadora1 + " deseas atacar o curarte");
+        string aoc = Console.ReadLine();
+        if (aoc == "atacar")
+        {Console.WriteLine("\n");
+            vidajugadora2 = vidajugadora2 - ataque1;
+            Console.WriteLine("Ahora la vida de " + jugadora2 + " es: " + vidajugadora2);
+        }
+        else if (aoc == "curar")
+
+        {
+            vidajugadora1 = vidajugadora1 + curacion1;
+            if (vidajugadora1 > 67)
+            {
+             vidajugadora1 = 67;
+            }
+            Console.WriteLine("Ahora la vida de " + jugadora1 + " es: " + vidajugadora1);
+        }
+        else
+        {
+            Console.WriteLine("Porfavor ingresa si deseas atacar o curarte");
+        }
+        Console.WriteLine("\n");
+        Console.WriteLine(jugadora2 + " Comienza");
+        Console.WriteLine("\n");
+        //Mostrar que puede atacar o curarse en cada ronda
+        //(Si la entrada no es correcta, indicar que vuelva a ingresarla)
+        Console.WriteLine(jugadora2 + " deseas atacar o curarte");
+        string aoc1 = Console.ReadLine();
+
+        if (aoc1 == "atacar")
+
+        {   Console.WriteLine("\n");
+            vidajugadora1 = vidajugadora1 - ataque2;
+
+            Console.WriteLine("Ahora la vida de " + jugadora1 + " es: " + vidajugadora1);
+        }
+        else if (aoc1 == "curar")
+
+        {
+            vidajugadora2 = vidajugadora2 + curacion2;
+            if (vidajugadora2 > 67)
+            {
+                vidajugadora2 = 67;
+            }
+            Console.WriteLine("Ahora la vida de " + jugadora2 + " es: " + vidajugadora2);
+        }
+        else
+        {
+            Console.WriteLine("Porfavor ingresa si deseas atacar o curarte");
+        }
+    }
+    else if (x == 2)
+    {
+        Console.WriteLine("\n");
+        Console.WriteLine(jugadora2 + " Comienza");
+        Console.WriteLine("\n");
+        //Mostrar que puede atacar o curarse en cada ronda
+        //(Si la entrada no es correcta, indicar que vuelva a ingresarla)
+        Console.WriteLine(jugadora2 + " deseas atacar o curarte");
+        string aoc1 = Console.ReadLine();
+        if (aoc1 == "atacar")
+
+        { Console.WriteLine("\n");
+            vidajugadora1 = vidajugadora1 - ataque2;
+
+            Console.WriteLine("Ahora la vida de " + jugadora1 + " es: " + vidajugadora1);
+        }
+        else if (aoc1 == "curar")
+
+        {
+            vidajugadora2 = vidajugadora2 + curacion2;
+            if (vidajugadora2 > 67)
+            {
+                vidajugadora2 = 67;
+            }
+            Console.WriteLine("Ahora la vida de " + jugadora2 + " es: " + vidajugadora2);
+        }
+        else
+        {
+            Console.WriteLine("Porfavor ingresa si deseas atacar o curarte");
+        }
+        Console.WriteLine("\n");
+        Console.WriteLine(jugadora1 + " Comienza");
+        Console.WriteLine("\n");
+        Console.WriteLine(jugadora1 + " deseas atacar o curarte");
+        string aoc = Console.ReadLine();
+        if (aoc == "atacar")
+
+        {           Console.WriteLine("\n");
+            vidajugadora2 = vidajugadora2 - ataque1;
+            Console.WriteLine("Ahora la vida de " + jugadora2 + " es: " + vidajugadora2);
+        }
+        else if (aoc == "curar")
+
+        {
+            vidajugadora1 = vidajugadora1 + curacion1;
+            if (vidajugadora1 > 67)
+            {
+                vidajugadora1 = 67;
+            }
+            Console.WriteLine("Ahora la vida de " + jugadora1 + " es: " + vidajugadora1);
+        }
+        else
+        {
+            Console.WriteLine("Porfavor ingresa si deseas atacar o curarte");
+        }
+    }
+    Console.WriteLine("\n");
+}
+if (vidajugadora1 < 0)
+{
+    Console.WriteLine(jugadora1 + " ha perdido");
+}
+else if (vidajugadora2 < 0)
+{
+    Console.WriteLine(jugadora2 + " ha perdido");
+}
+Console.WriteLine("\n\n");
+Console.WriteLine("¡GRACIAS POR JUGAR!");
 
